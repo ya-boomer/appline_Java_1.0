63034083f4b42ccf1f00f98204fc3dfcf031cdbf
Mishchenko Aleksey <mishchenko.an.26@gmail.com>
2020-08-25 17:22:41 +0300

    Создан новый класс перевода двоичных значений в 10-тизначную систему исчесления

---
+++ src/Bin2Decim.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Bin2Decim {
+    public static void main(String[] args) {
+        //Вводим любое значение с клавиатуры
+        System.out.println("Введите значение:");
+        Scanner in1 = new Scanner(System.in);
+        int number = in1.nextInt();
+        System.out.println(toDec(String.valueOf(number)));
+
+    }
+
+    //Цикл с преобразованием полученного числа в десятичную систему.
+    static int toDec(String base2) {
+        char[] chars = base2.toCharArray();
+        int result = 0;
+        int mult = 1;
+        for (int i = base2.length()-1; i >= 0; i--) {
+            if (chars[i]=='1') {
+                result += mult;
+            }
+            if (chars[i]=='-') //Проверка на отрицательное значение
+            {
+                result *= (-1);
+            }
+            mult*=2;
+        }
+        return result;
+    }
+}
