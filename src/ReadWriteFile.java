020ee4183f1934b9399e44675bf4e66d3505f7f9
Mishchenko Aleksey <mishchenko.an.26@gmail.com>
2020-09-10 09:49:28 +0300

    Создал класс ReadWriteFile, который считывает файл, затем получает от пользователя информацию и перезаписывает в файл, количество строк ограничено количеством строк из первоначального файла.

---
+++ src/ReadWriteFile.java
@@ -0,0 +1,65 @@
+import java.io.*;
+
+/**
+ * Задача №18:  * Необходимо написать программу, которая будет выводить на экран текстовые данные из файла .txt,
+ * а потом в этот же файл перезаписывать текстовые данные, введенные вручную. Количество строк после
+ * перезаписи должно быть столько же, сколько и в изначальном варианте.
+ */
+public class ReadWriteFile {
+    private static String readString;
+    private static BufferedReader br;
+//BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
+
+
+    public static void main(String[] args) throws IOException {
+
+
+        // проверяем существует ли указанный файл и выводим содержимое в консоль
+
+        System.out.println("Файл содержит " + getNumberLine() + " строк");
+            getFileString();
+            setFileString(getNumberLine());
+    }
+
+    public static int getNumberLine() throws IOException {
+        int i = 0;
+        {
+            try (BufferedReader br = new BufferedReader(new FileReader("src/ReadWriteFile.txt"))) {
+                while ((readString = br.readLine()) != null) {
+                    i++;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return i;
+    }
+
+    public static void getFileString() throws IOException {
+        System.out.println("Файл имеет следущее содержимое:");
+        try (BufferedReader br = new BufferedReader(new FileReader("src/ReadWriteFile.txt"))){
+            while ((readString = br.readLine()) != null) {
+                System.out.println(readString);
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        }
+
+
+    public static void setFileString(int lineCount) throws IOException {
+        String writeString;
+        try (FileWriter fw = new FileWriter("src/ReadWriteFile.txt")) {
+            BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Внимание! Информация будет перезаписана");
+            for (int j = 0; j < lineCount; j++) {
+                writeString = buf.readLine();
+                writeString = writeString + System.lineSeparator();
+                fw.write(writeString);
+            }
+        }
+    }
+}
+
