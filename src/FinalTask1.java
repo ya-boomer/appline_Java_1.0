920309050b95f73f27bd816ba339ee01b3583a79
Mishchenko Aleksey <mishchenko.an.26@gmail.com>
2020-09-11 15:54:13 +0300

    В классе FinalTask1 добавил проверку на то, что пользователь ввёл бинарное значение

--- src/FinalTask1.java
+++ src/FinalTask1.java
@@ -13,13 +13,19 @@ public class FinalTask1 {
         //считываем бинарное значение в строку
         Scanner in1 = new Scanner(System.in);
         bin = in1.nextLine();
-
-        int n = 0;
-        // Производим перевод бинарного значения в десятичное
-        for (int i = bin.length() - 1, j = 1; i >= 0; i--, j *= 2)
-            n += (bin.charAt(i) - '0') * j;
-        // Выводим полученный результат
-        System.out.println("Полученное число = " + n);
+        int index = bin.indexOf('0');
+        int index1 = bin.indexOf('1');
+        if (index == -1 || index1 ==-1) {
+            System.out.println("Введено не бинарное значение");
+        }
+        else {
+            int n = 0;
+            // Производим перевод бинарного значения в десятичное
+            for (int i = bin.length() - 1, j = 1; i >= 0; i--, j *= 2)
+                n += (bin.charAt(i) - '0') * j;
+            // Выводим полученный результат
+            System.out.println("Полученное число = " + n);
+        }
     }
 }
 
